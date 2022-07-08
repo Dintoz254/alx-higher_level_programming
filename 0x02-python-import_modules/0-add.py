69f7392eb50ddd52ecd1b00cc5103f977e1b2bec
dintoz254 <shadrackolungo@gmail.com>
2022-07-08 03:15:15 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as add_module
-    a = 1
-    b = 2
-    add = add_module.add(a, b)
-=======
-#!/usr/bin/pytho3
-
-if __name__ == "__main__":
-    a = 1
-    b = 2
-
     from add_0 import add
->>>>>>> 6d8299ae2bff1356129e252904a21260dddddb51
+    a = 1;
+    b = 2;
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
